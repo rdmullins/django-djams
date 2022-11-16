9347d4c9d46502731d9654c500bbf79b4d72f3e2
Roger Mullins <roger.mullins@student.awesomeinc.org>
2022-11-16 18:10:54 +0000

    Added Album URLs

--- djams/musicdb/urls.py
+++ djams/musicdb/urls.py
@@ -4,4 +4,6 @@ from .views import *
 urlpatterns = [
     path("playlist/", PlaylistAPIView.as_view()),
     path("playlist/<str:pk>/", PlaylistAPIView.as_view()),
+    path("album/", AlbumAPIView.as_view()),
+    path("album/<str:pk>/", AlbumAPIView.as_view()),
 ]
