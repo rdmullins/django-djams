2f252851cc806a45217cefd065c3d38f76fbce2d
Roger Mullins <roger.mullins@student.awesomeinc.org>
2022-11-16 16:11:37 +0000

    Added Read option

--- djams/musicdb/views.py
+++ djams/musicdb/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http.response import Http404
+from rest_framework.views import APIView
+from .models import *
+from .serializers import *
+from rest_framework.response import Response
 
 # Create your views here.
+class PlaylistAPIView(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Playlist.objects.get(pk=pk)
+        except Playlist.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk=None, format=None):
+        if pk:
+            data = self.get_object(pk)
+            serializer = PlaylistSerializer(data)
+        else:
+            data = Playlist.objects.all()
+            serializer = PlaylistSerializer(data, many=True)
+
+            return Response(serializer.data)
